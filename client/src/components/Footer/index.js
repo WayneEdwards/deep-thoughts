57dd6fcfaee66296605b9fc6ee34c0212a438e18
Wayne Edwards <waedwards42@yahoo.com>
2021-07-27 23:53:20 -0700

    created client side

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2021 by Wayne Edwards</div>
+    </footer>
+  );
+};
+
+export default Footer;
